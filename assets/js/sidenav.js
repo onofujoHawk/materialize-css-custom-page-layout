9b7fbafca32deb391d6b2ead0f6f4dc416bca3a2
Andrea Di Marco <Andrea Di Marco>
2016-12-12 13:46:10 +0100

    First commit

---
+++ assets/js/sidenav.js
@@ -0,0 +1,7 @@
+$(document).ready(function($) {
+   //al click di button collapse, attiva la sidenav
+   $(".button-collapse").sideNav();
+   $('.parallax').parallax();
+	 $('.slider').slider({full_width: true});
+
+}); // end of document ready
